b3a5139113015a7f8d0233a36a393eab61db3810
KimWonseop <won0539@gmail.com>
2023-04-30 16:45:20 +0900

    Update README.md and delete useless files

--- Assets/Scripts/UI/InGameView.cs
+++ Assets/Scripts/UI/InGameView.cs
@@ -9,13 +9,9 @@ namespace FPS.UI {
         [SerializeField] private Slider playerHealthBar;
         [SerializeField] private Slider bossHealthBar;
         [SerializeField] private Slider bulletCountBar;
-        [SerializeField] private TextMeshProUGUI gameTimer;
-        [SerializeField] private TextMeshProUGUI playerNickname;
-
+        
         public Slider PlayerHealthBarBar => playerHealthBar;
         public Slider BossHealthBarBar => bossHealthBar;
         public Slider BulletCountBar => bulletCountBar;
-        public TextMeshProUGUI GameTimer => gameTimer;
-        public TextMeshProUGUI PlayerNickname => playerNickname;
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# fps_unity_example
-Unity-FPS 게임 토이프로젝트
+## Unity FPS Example
+### Description
+- Unity의 Input System을 이용하여 캐릭터와 카메라 이동 개발.
+- ObjectPool을 이용한 Bullet 오브젝트 재사용
+
+### Technology Stack
+- Unity 2021.3.4f1
+  -  Input System
+  
+- C#
+  - ObjectPooling
+  - Singleton
+
+
